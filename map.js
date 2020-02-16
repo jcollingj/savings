3f235608103f6f04094d1281f555f95f3542f36a
jcollingj <jcollingj@gmail.com>
2020-02-16 09:57:04 -0500

    Update map.js

--- map.js
+++ map.js
@@ -7,10 +7,11 @@
       console.log(zipCode);
       numberZipCode =parseInt(zipCode);
       console.log(numberZipCode);
+
       switch (zipCode) {
-case 75050: elChild.innerHTML = '<iframe src="https://www.google.com/maps/d/embed?mid=1w-S6gCaj_4kBfkCAzwR9dU2yfu8" width="100%" height="480"></iframe>'; break;
-case 75051: elChild.innerHTML = '<iframe src="https://www.google.com/maps/d/embed?mid=1_D4-xnaXSPKzx7mllISBRDiyJTI" width="100%" height="480"></iframe>'; break;
-case 75052: elChild.innerHTML = '<iframe src="https://www.google.com/maps/d/embed?mid=1IrKCoWGy2wrd2OWqRPp-qFJMb7M" width="100%" height="480"></iframe>'; break;
+        case 75050: console.log("I hate myself"); break;
+        case 75051: console.log("I hate myself more"); break;
+
       };
       
       
