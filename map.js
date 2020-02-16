387c0ecf26c958a79de078d3d4b9068512c6573a
jcollingj <jcollingj@gmail.com>
2020-02-16 09:53:26 -0500

    Update map.js

--- map.js
+++ map.js
@@ -5,6 +5,8 @@
       
       var zipCode = $(this).attr('id');
       console.log(zipCode);
+      numberZipCode =parseInt(zipCode);
+      console.log(numberZipCode);
       switch (zipCode) {
 case 75050: elChild.innerHTML = '<iframe src="https://www.google.com/maps/d/embed?mid=1w-S6gCaj_4kBfkCAzwR9dU2yfu8" width="100%" height="480"></iframe>'; break;
 case 75051: elChild.innerHTML = '<iframe src="https://www.google.com/maps/d/embed?mid=1_D4-xnaXSPKzx7mllISBRDiyJTI" width="100%" height="480"></iframe>'; break;
