1dc3a5cb7ba6a02d40711d6e21ac744b90c8ef54
jcollingj <jcollingj@gmail.com>
2020-02-16 09:13:57 -0500

    Update map.js

--- map.js
+++ map.js
@@ -2,6 +2,8 @@
 
   $(document).ready(function() {
     $('#my-unique-link').on('click', function(e) {
-       elChild.innerHTML = '<iframe src="https://www.google.com/maps/d/embed?mid=1w-S6gCaj_4kBfkCAzwR9dU2yfu8" width="100%" height="480"></iframe>'
+      console.log("Does this work");
+      var id = $(this).attr('id');
+      console.log(id);
     });
   });
